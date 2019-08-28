9e3de147f6e0475324d00b01d6c217a4931902bc
harshpatel <patel-h91@webmail.uwinnipeg.ca>
2019-08-28 04:11:00 -0500

    added test cases

--- driver.py
+++ driver.py
@@ -16,6 +16,29 @@ sh_and_m = [
     [[3, 3, 3], 3],
 ]
 
+sh_and_m = [[[1 for _ in range(i)], i] for i in range(2, 11)]
+
+sh_and_m = [
+    [[2, 1], 2],
+    [[3, 1], 2],
+    [[4, 1], 2],
+    [[5, 1], 2],
+    [[2, 1, 1], 3],
+    [[3, 1, 1], 3],
+    [[4, 1, 1], 3],
+    [[5, 1, 1], 3],
+    [[2, 1, 1, 1], 4],
+    [[3, 1, 1, 1], 4],
+    [[4, 1, 1, 1], 4],
+    [[5, 1, 1, 1], 4],
+    [[2, 1, 1, 1, 1], 5],
+    [[3, 1, 1, 1, 1], 5],
+    [[4, 1, 1, 1, 1], 5],
+    [[5, 1, 1, 1, 1], 5],
+]
+
+sh_and_m = [[[2, 1, 1, 1, 1], 5]]
+
 for sm in sh_and_m:
     print('shape: ' + str(sm[0]))
     print('m: ' + str(sm[1]))
@@ -26,4 +49,3 @@ for sm in sh_and_m:
     evaluation = o.evaluate(polynomial, q, o.nth_root_of_unity(2*sm[1]))
     print('p(2m^th root) = ' + str(evaluation))
     print()
-
